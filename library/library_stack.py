d352a7ce863985669fa53e9cf16cd1014f5deb52
Fayomi Fashanu <f.fashau@hotmail.co.uk>
2021-12-21 16:10:04 +0000

    formatted code into functions

--- library/library_stack.py
+++ library/library_stack.py
@@ -14,66 +14,79 @@ class LibraryStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create library table
         table = dynamodb.Table(self, "books-table",
                 partition_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING)
                 )
-        
-        # POST
-        post_fn = lambda_.Function(self, "post",
+
+        # Create book post function 
+        def post_book():
+            post_fn = lambda_.Function(self, "post",
             runtime=lambda_.Runtime.PYTHON_3_7,
             handler="post.main",
             code=lambda_.Code.from_asset("./lambda/post")
         )
+            # grant reand and write permissions to the table
+            table.grant_read_write_data(post_fn)
+            post_fn.add_environment("TABLE_NAME", table.table_name)
 
+            return post_fn
 
-        table.grant_read_write_data(post_fn)
-        post_fn.add_environment("TABLE_NAME", table.table_name)
-
-
-        # GET
-        get_fn = lambda_.Function(self, "get",
-            runtime=lambda_.Runtime.PYTHON_3_7,
-            handler="get.main",
-            code=lambda_.Code.from_asset("./lambda/get")
-        )
-
-
-        table.grant_read_write_data(get_fn)
-        get_fn.add_environment("TABLE_NAME", table.table_name)
-
-
-        # API
-        api = apigateway.LambdaRestApi(self, "library-api", endpoint_configuration=apigateway.EndpointConfiguration(
-        types=[apigateway.EndpointType.REGIONAL]),
-            handler=get_fn,
-            proxy=False
-        )
-        books = api.root.add_resource("books")
         
-        post_book_integration = apigateway.LambdaIntegration(post_fn, proxy=False, integration_responses=[apigateway.IntegrationResponse(
-            status_code="200")])
-        get_book_integration = apigateway.LambdaIntegration(get_fn, proxy=False, integration_responses=[apigateway.IntegrationResponse(
-            status_code="200")])
-
-        books.add_method("GET", get_book_integration, method_responses=[apigateway.MethodResponse(
-        # Successful response from the integration
-        status_code="200"
-    )]) # GET /books
-        books.add_method("POST", post_book_integration, method_responses=[apigateway.MethodResponse(
-        # Successful response from the integration
-        status_code="200"
-    )]) 
-    # POST /books
+        # Create book post function 
+        def get_books():
+            get_fn = lambda_.Function(self, "get",
+                runtime=lambda_.Runtime.PYTHON_3_7,
+                handler="get.main",
+                code=lambda_.Code.from_asset("./lambda/get")
+            )
 
 
-# Test
+            table.grant_read_write_data(get_fn)
+            get_fn.add_environment("TABLE_NAME", table.table_name)
+
+            return get_fn
+        
+        # Create API function
+        def create_api(get_fn, post_fn):
+            api = apigateway.LambdaRestApi(self, "library-api", endpoint_configuration=apigateway.EndpointConfiguration(
+            types=[apigateway.EndpointType.REGIONAL]),
+                handler=get_fn,
+                proxy=False
+            )
+            books = api.root.add_resource("books")
+        
+            post_book_integration = apigateway.LambdaIntegration(post_fn, proxy=False, integration_responses=[apigateway.IntegrationResponse(
+                status_code="200")])
+            get_book_integration = apigateway.LambdaIntegration(get_fn, proxy=False, integration_responses=[apigateway.IntegrationResponse(
+                status_code="200")])
+
+            books.add_method("GET", get_book_integration, method_responses=[apigateway.MethodResponse(
+            # Successful response from the integration
+            status_code="200"
+            )]) # GET /books
+            books.add_method("POST", post_book_integration, method_responses=[apigateway.MethodResponse(
+            # Successful response from the integration
+            status_code="200"
+            )]) 
+
+
+
+        post_book = post_book()
+        get_books = get_books()
+        create_api(get_books, post_book)
+
+
 # CREATE POST AND GET FUNCTIONS
 # ADD LOCATION COLUMN FOR BOOKS (e.g here or at friends house)
 
 # ADD API SECURITY (API KEY? COGNITO?)
+# - https://awskarthik82.medium.com/part-1-securing-aws-api-gateway-using-aws-cognito-oauth2-scopes-410e7fb4a4c0 
+# - https://www.youtube.com/watch?v=4n5Ssr3NZRc
 
 # DOMAIN NAME
+
+# CREATE GITHUB PIPELINE
 
 
 
